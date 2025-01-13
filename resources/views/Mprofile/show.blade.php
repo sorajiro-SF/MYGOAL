d4b565e1409f679571a5992439481830e9306ce5
sorajiro-SF <tengtiantianya@gmail.com>
2025-01-13 12:30:33 +0000

    deploy commit 4

--- resources/views/Mprofile/show.blade.php
+++ resources/views/Mprofile/show.blade.php
@@ -4,25 +4,27 @@
             {{ __('プロフィール詳細') }}
         </h2>
         <style>
-    </style>
+        </style>
     </x-slot>
     <br></br>
     <div class="show">
-            <figure class="image">
-                <img class="profile_show_img" src="{{asset('storage/image/'. $pro->image_path)}}" alt="">
-            </figure>
-            <div class="right">
+        <figure class="image">
+            <img class="profile_show_img" src="{{asset('storage/image/'. $pro->image_path)}}" alt="">
+        </figure>
+        <div class="right">
             <p class="name">名前：{{$pro->name}}（{{$pro->name_kana}}）</p>
-            <p class="relation">関係性：{{$pro->relation->relation}}</p>
-            <p class="birth">誕生日：{{ $pro->birth }}</p>
-            <p class="from">出身地：{{$pro->from->from}}</p>
-            <p class="hobby">趣味：{{$pro->hobby}}</p>
-            <p class="food">好きな食べ物：{{$pro->food}}</p>
-            <p class="work">仕事：{{$pro->work}}</p>
-            <p class="word">好きな言葉：{{$pro->word}}</p>
-            </div>
-            </div>
+            <div class="desc">
+                <p class="relation">関係性：{{$pro->relation->relation}}</p>
+                <p class="birth">誕生日：{{ $pro->birth }}</p>
+                <p class="from">出身地：{{$pro->from->from}}</p>
+                <p class="hobby">趣味：{{$pro->hobby}}</p>
+                <p class="food">好きな食べ物：{{$pro->food}}</p>
+                <p class="work">仕事：{{$pro->work}}</p>
+                <p class="word">好きな言葉：{{$pro->word}}</p>
             </div>
         </div>
     </div>
-    </x-app-layout>
+    </div>
+    </div>
+    </div>
+</x-app-layout>
